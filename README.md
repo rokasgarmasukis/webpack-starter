0a591498c930864d7efc121fcede957bf1e26dfd
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-03-03 10:08:14 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Webpack App Starter
+
+Includes:
+- Webpack configuration using html template
+- Style and css loaders
+- Babel configuration
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import message from './message';
+import './css/style.css';
+
+console.log(message.text);
+
+const hello = () => console.log('Hi man');
+
+hello()
